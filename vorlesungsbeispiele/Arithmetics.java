dd7876655428bc7c98c3e0047d809a4b1eb261a2
Jan Eike Suchar <jan-eike.suchard@magenta.de>
2020-10-22 22:26:27 +0200

    New File

---
+++ vorlesungsbeispiele/Arithmetics.java
@@ -0,0 +1,20 @@
+/*
+ * Factorial
+ *
+ * Version 1.0
+ *
+ * Open Source
+ *
+ * Written by Jan Eike Suchard
+ */
+package vorlesungsbeispiele;
+
+public class Arithmetics {
+
+    public static void main(String[] args) {
+        int x = 5 + 4;
+        int y = 9 - 3 * 2;
+        int z = x / (y - 1);
+        System.out.println(z);
+    }
+}
