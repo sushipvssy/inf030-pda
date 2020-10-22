2bb4978ad00da8689bdf3e2ef267e4e13546932f
Jan Eike Suchar <jan-eike.suchard@magenta.de>
2020-10-22 23:19:14 +0200

    added while loop example

---
+++ vorlesungsbeispiele/WhileLoop.java
@@ -0,0 +1,13 @@
+package vorlesungsbeispiele;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 3) {
+            //Ersetzbar mit: x = x + 1;
+            x++;
+            System.out.println(x);
+        }
+    }
+}
