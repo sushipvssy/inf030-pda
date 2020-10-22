830bcce7edaf3e09bc2131afb5943dfb5d92617a
Jan Eike Suchar <jan-eike.suchard@magenta.de>
2020-10-22 21:13:14 +0200

    rename template and create factorial.java again

---
+++ vorlesungsbeispiele/Factorial.java
@@ -0,0 +1,15 @@
+package vorlesungsbeispiele;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = 1;
+        while (number > 0) {
+            result = result * number;
+            number--;
+        }
+        System.out.println(result);
+    }
+    
+}
